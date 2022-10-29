ad509e419c7ba2fe7a5a3d10df38730c5b9fe5ba
dhruvbhatt <bhatt.dhruv.s@gmail.com>
2022-10-28 21:31:15 -0400

    Initial Commit

---
+++ PlaySwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PlaySwiftUI
+//
+//  Created by Dhruv on 10/28/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PlaySwiftUI/PlaySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaySwiftUIApp.swift
+//  PlaySwiftUI
+//
+//  Created by Dhruv on 10/28/22.
+//
+
+import SwiftUI
+
+@main
+struct PlaySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
